a18b5e712ae9fe8997766fcca2b79bec2947a126
lenimitoundie <7kurz@informatik.uni-hamburg.de>
2019-05-22 14:38:02 +0200

    new decision for goal search, now only for specified search-time

---
+++ bitbots_body_behavior/src/bitbots_body_behavior/decisions/in_goal_search_time.py
@@ -0,0 +1,18 @@
+import rospy
+
+from dynamic_stack_decider.abstract_decision_element import AbstractDecisionElement
+
+
+class InGoalSearchTime(AbstractDecisionElement):
+    def __init__(self, blackboard, dsd, parameters=None):
+        super(InGoalSearchTime, self).__init__(blackboard, dsd, parameters)
+        self.outrun_goal_search_time = self.blackboard.config['goal_search_duration']
+        self.start_searching_time = rospy.get_time()
+
+    def perform(self, reevaluate=False):
+        if rospy.get_time() - self.start_searching_time < self.outrun_goal_search_time:
+            return 'YES'
+        return 'NO'
+
+    def get_reevaluate(self):
+        return True
